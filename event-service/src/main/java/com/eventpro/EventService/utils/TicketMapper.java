73b761b625c025805d966e852c58172726b397e0
RobsonPierno <robson.pierno@gmail.com>
2025-11-03 14:34:03 +0100

    TicketMapper

---
+++ event-service/src/main/java/com/eventpro/EventService/utils/TicketMapper.java
@@ -0,0 +1,23 @@
+package com.eventpro.EventService.utils;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.eventpro.EventService.dto.TicketDTO;
+import com.eventpro.EventService.model.Ticket;
+
+@Mapper(componentModel = "spring", 
+	imports = {
+        com.eventpro.EventService.enums.TicketType.class,
+        com.eventpro.EventService.model.Event.class
+    })
+public interface TicketMapper {
+
+    @Mapping(target = "type", expression = "java(dto.status() != null ? TicketType.valueOf(dto.type().toUpperCase()) : null)")
+    @Mapping(target = "event", expression = "java(dto.eventId() != null ? new Event(dto.eventId()) : null)")
+    Ticket toEntity(TicketDTO dto);
+
+    @Mapping(target = "type", expression = "java(entity.getStatus() != null ? entity.getStatus().name() : null)")
+    @Mapping(target = "eventId", expression = "java(entity.getEvent() != null ? entity.getEvent().getId() : null)")
+    TicketDTO toDto(Ticket entity);
+}
