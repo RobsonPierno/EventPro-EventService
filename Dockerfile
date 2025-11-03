c5fe18e895df67ffd0c83016d09effbdf5df2fdd
RobsonPierno <robson.pierno@gmail.com>
2025-11-03 10:33:26 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:25-jdk-slim
+RUN mkdir /app
+WORKDIR /app
+COPY event-service/target/event-service-0.0.1-SNAPSHOT.jar /app/app.jar
+CMD ["java", "-jar", "/app/app.jar"]
---
+++ event-service/src/test/java/com/eventpro/EventService/EventServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.eventpro.EventService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EventServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
